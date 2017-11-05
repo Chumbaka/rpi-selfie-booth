a33d0b4ae3961c931635892ae8327ba7f17f62c6
root <chumbakas@yes.my>
2017-11-05 16:37:13 +0000

    more small updates

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ CONSUMER_SECRET = ""
 ACCESS = ""
 ACCESS_SECRET = ""
 
+#TODO use the official gpiozero guide link
 #Button. Refer to https://www.raspberrypi.org/blog/gpio-zero-a-friendly-python-api-for-physical-computing/
-BUTTON = 2
+BUTTON = 4
--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import config
 import picamera
+import sys
 from twython import Twython
 from PIL import Image
 from time import sleep
 from gpiozero import Button
-
-button = Button(config.BUTTON)
 
 #camera = picamera.PiCamera()
 
 # img = Image.open("foo.jpg")
 
 # overlol = camera.add_overlay(img.tobytes(), size=img.size)
+#overlol.layer = 3
+#alpha = 0 is transparent, and vice versa
+#overlol.alpha = 0
 
 twitter = Twython(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS, config.ACCESS_SECRET)
 
 while True:
 
-	#camera.start_preview()
+	#camera.start_preview(fullscreen=false, window=(x y height width))
 	#camera.capture("bar.jpg")
 	#camera.stop_preview()
 	#photo = open("bar.jpg", "rb")
+	Button(config.BUTTON).wait_for_press()
 
 	try:
 		twitter.update_status_with_media(media=photo, status="Testing, testing. Anyone can see me?")
 		#TODO: Upload complete picture
+		# feh -x filename
 		sleep(5)
 	except:
 		#TODO: Upload Failed & check out the twitter acc. picture 
